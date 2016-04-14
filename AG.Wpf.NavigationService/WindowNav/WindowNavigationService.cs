463d7f69ea13f00c215f673e456da3ecc9145884
Angel Guevara, Jr <aguevara.716@gmail.com>
2016-04-14 18:32:42 -0400

    Added logic to set the windows' owner when initializing the window navigation service

--- AG.Wpf.NavigationService/WindowNav/WindowNavigationService.cs
+++ AG.Wpf.NavigationService/WindowNav/WindowNavigationService.cs
@@ -7,8 +7,22 @@ namespace AG.Wpf.NavigationService.WindowNav
 {
     public class WindowNavigationService : IWindowNavigationService
     {
+        private readonly Func<Window> MAIN_WINDOW_GETTER;
         private readonly Dictionary<string, Func<Window>> windowsByKey = new Dictionary<string, Func<Window>>();
+        private Window mainWindow;
         public object WindowParameter { get; private set; }
+
+        public WindowNavigationService(Func<Window> mainWindowGetter = null)
+        {
+            MAIN_WINDOW_GETTER = mainWindowGetter;
+        }
+
+        private Window GetMainWindow()
+        {
+            if (mainWindow == null && MAIN_WINDOW_GETTER != null)
+                mainWindow = MAIN_WINDOW_GETTER();
+            return mainWindow;
+        }
 
         public void OpenWindow(string key, bool isTopMost, bool isDialog)
         {
@@ -24,7 +38,7 @@ namespace AG.Wpf.NavigationService.WindowNav
                 WindowParameter = parameter;
                 var window = windowsByKey[key].Invoke();
                 window.Topmost = isTopMost;
-                //TODO figure out how to set the owner
+                window.Owner = GetMainWindow();
                 if (isDialog == true)
                     window.ShowDialog();
                 else
@@ -39,7 +53,7 @@ namespace AG.Wpf.NavigationService.WindowNav
                 if (windowsByKey.ContainsKey(key) == true)
                     throw new ArgumentException($"This key has already been used: {key}", nameof(key));
                 if (windowsByKey.Any(w => w.Value == ctor) == true)
-                    throw new ArgumentException($"This type has already been configured with key {windowsByKey.First(v => v.Value == ctor).Key}", nameof(ctor));
+                    throw new ArgumentException($"This type has already been configured with key \"{windowsByKey.First(v => v.Value == ctor).Key}\"", nameof(ctor));
                 windowsByKey.Add(key, ctor);
             }
         }
